4de07f6a86d56ebda364a7f1ce8707c805eb52c8
Alec West <alecwest38@gmail.com>
2018-03-12 18:53:24 -0500

    Add component gens for music

--- routes/music/src/app/app.module.ts
+++ routes/music/src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import {
+  LocationStrategy,
+  HashLocationStrategy,
+  APP_BASE_HREF
+} from '@angular/common';
 
 
 import { AppComponent } from './app.component';
+import { ArtistComponent } from './artist/artist.component';
+import { AlbumComponent } from './album/album.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule
