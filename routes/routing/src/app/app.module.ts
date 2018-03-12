4dc5b1bc7963221ecd37cd0519bd4e4178be7418
Alec West <alec.west@adtran.com>
2018-03-12 16:53:58 -0500

    Get to routing strategies

---
+++ routes/routing/src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { 
+  RouterModule, 
+  Routes
+} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'contactus', redirectTo: 'contact' },
+
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ LoggedInGuard ]
+  },
+
+  {
+    path: 'products',
+    component: ProductsComponent,
+    children: childRoutes
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    ProtectedComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
+    ProductsModule
+  ],
+  providers: [
+    // { provide: LocationStrategy, useClass: HashLocationStrategy },
+    AUTH_PROVIDERS,
+    LoggedInGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
