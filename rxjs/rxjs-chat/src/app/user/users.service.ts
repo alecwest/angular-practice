c18559f860a5be8888f7eedb1ee2208d2d24ffcd
Alec West <alecwest38@gmail.com>
2018-03-18 21:15:28 -0500

    Complete UsersService

---
+++ rxjs/rxjs-chat/src/app/user/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { User } from './user.model';
+
+@Injectable()
+export class UsersService {
+  currentUser: Subject<User> = new BehaviorSubject<User>(null);
+
+  constructor() { }
+
+  public setCurrentUser(newUser: User): void {
+    this.currentUser.next(newUser);
+  }
+}
+
+export const userServiceInjectables: Array<any> = [
+  UsersService
+];
