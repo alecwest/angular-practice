402c9375fa6e6655bc64e7eabe7adefc5c7304ec
Alec West <alecwest38@gmail.com>
2018-03-19 18:18:08 -0500

    Complete messagesService

--- rxjs/rxjs-chat/src/app/message/messages.service.spec.ts
+++ rxjs/rxjs-chat/src/app/message/messages.service.spec.ts
@@ -1,12 +1,45 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MessagesService } from './messages.service';
+import { Message } from './message.model';
+import { Thread } from '../thread/thread.model';
+import { User } from '../user/user.model';
 
 describe('MessagesService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [MessagesService]
     });
+  });
+
+  it('should test', () => {
+    const user: User = new User('Nate', '');
+    const thread: Thread = new Thread('t1', 'Nate', '');
+    const m1: Message = new Message({
+      author: user,
+      text: 'Hi!',
+      thread: thread
+    });
+    const m2: Message = new Message({
+      author: user,
+      text: 'Bye!',
+      thread: thread
+    });
+
+    const messagesService: MessagesService = new MessagesService();
+
+    messagesService.newMessages
+      .subscribe((message: Message) => {
+        console.log('=> newMessages: ' + message.text);
+      });
+
+    messagesService.messages
+      .subscribe((messages: Message[]) => {
+        console.log('=> messages: ' + messages.length);
+      });
+
+    messagesService.addMessage(m1);
+    messagesService.addMessage(m2);
   });
 
   it('should be created', inject([MessagesService], (service: MessagesService) => {
--- rxjs/rxjs-chat/src/app/message/messages.service.ts
+++ rxjs/rxjs-chat/src/app/message/messages.service.ts
@@ -4,12 +4,52 @@ import { Message } from './message.model';
 import { User } from '../user/user.model';
 import { Thread } from '../thread/thread.model';
 
+const initialMessages: Message[] = [];
+
+interface IMessagesOperation extends Function {
+  (messages: Message[]): Message[];
+}
+
 @Injectable()
 export class MessagesService {
   newMessages: Subject<Message> = new Subject<Message>();
   messages: Observable<Message[]>;
+  updates: Subject<any> = new Subject<any>();
+  create: Subject<Message> = new Subject<Message>();
+  markThreadAsRead: Subject<any> = new Subject<any>();
 
-  constructor() { }
+  constructor() {
+    this.messages = this.updates
+      .scan((messages: Message[], operation: IMessagesOperation) => {
+        return operation(messages);
+      },
+    initialMessages)
+    .publishReplay(1)
+    .refCount();
+
+    this.create
+      .map(function(message: Message): IMessagesOperation {
+        return (messages: Message[]) => {
+          return messages.concat(message);
+        };
+      })
+      .subscribe(this.updates);
+
+    this.newMessages.subscribe(this.create);
+
+    this.markThreadAsRead
+      .map((thread: Thread) => {
+        return (messages: Message[]) => {
+          return messages.map((message: Message) => {
+            if (message.thread.id === thread.id) {
+              message.isRead = true;
+            }
+            return message;
+          });
+        };
+      })
+      .subscribe(this.updates);
+  }
 
   addMessage(message: Message): void {
     this.newMessages.next(message);
@@ -23,3 +63,7 @@ export class MessagesService {
       });
   }
 }
+
+export const messagesServiceInjectables: Array<any> = [
+  MessagesService
+];
