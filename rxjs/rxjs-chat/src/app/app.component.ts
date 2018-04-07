53705dc6f8d9a1e85ffb9b1ef2ea2ad4b51278d0
Alec West <alecwest38@gmail.com>
2018-04-06 20:42:28 -0500

    Add app-root constructor

--- rxjs/rxjs-chat/src/app/app.component.ts
+++ rxjs/rxjs-chat/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ChatExampleData } from './data/chat-example-data';
-
+import { MessagesService } from './message/messages.service';
+import { ThreadsService } from './thread/threads.service';
+import { UsersService } from './user/users.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,7 @@ import { ChatExampleData } from './data/chat-example-data';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(public messagesService: MessagesService, public threadsService: ThreadsService, public usersService: UsersService) {
+    ChatExampleData.init(messagesService, threadsService, usersService);
+  }
 }
