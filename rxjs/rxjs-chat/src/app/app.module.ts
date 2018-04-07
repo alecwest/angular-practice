f06e1af2b00dfb145171ef6643fff5364d846cf7
Alec West <alecwest38@gmail.com>
2018-04-06 21:06:56 -0500

    Add chat-thread

--- rxjs/rxjs-chat/src/app/app.module.ts
+++ rxjs/rxjs-chat/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatThreadsComponent
+    ChatThreadsComponent,
+    ChatThreadComponent
   ],
   imports: [
     BrowserModule
