ed78c80522a51236b34d2787ed5351c652516ac8
Alec West <alecwest38@gmail.com>
2018-04-06 20:40:25 -0500

    Revert "Add app-root constructor"
    This reverts commit 79ec7f0430da3221c40d163da2bafe3109e1fe39.

--- rxjs/rxjs-chat/src/app/app.component.ts
+++ rxjs/rxjs-chat/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { ChatExampleData } from './data/chat-example-data';
-import { MessagesService } from './message/messages.service';
-import { ThreadsService } from './thread/threads.service';
-import { UsersService } from './user/users.service';
+
 
 @Component({
   selector: 'app-root',
@@ -10,7 +8,5 @@ import { UsersService } from './user/users.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(public messagesService: MessagesService, public threadsService: ThreadsService, public usersService: UsersService) {
-    ChatExampleData.init(messagesService, threadsService, usersService);
-  }
+  title = 'app';
 }
