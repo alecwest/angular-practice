db61a760d555ff4f11dbd6706f0173a3c344ab67
Alec West <alecwest38@gmail.com>
2018-04-06 21:02:50 -0500

    Add initial content to chat-thread

--- rxjs/rxjs-chat/src/app/chat-threads/chat-threads.component.ts
+++ rxjs/rxjs-chat/src/app/chat-threads/chat-threads.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Thread } from '../thread/thread.model';
+import { ThreadsService } from '../thread/threads.service';
 
 @Component({
-  selector: 'app-chat-threads',
+  selector: 'chat-threads',
   templateUrl: './chat-threads.component.html',
   styleUrls: ['./chat-threads.component.css']
 })
 export class ChatThreadsComponent implements OnInit {
+  threads: Observable<any>;
 
-  constructor() { }
+  constructor(public threadsService: ThreadsService) {
+    this.threads = threadsService.orderedThreads;
+  }
 
   ngOnInit() {
   }
